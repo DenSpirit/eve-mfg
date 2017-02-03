298c33be296cee88c7a473764f094e488cd08ed8
DenSpirit <elfmax@tut.by>
2017-02-03 16:44:51 +0300

    remove targets and adjusting poms

--- eve-blueprints-maven-plugin/src/main/java/eve/apol/eveblueprints/BlueprintGraphMojo.java
+++ eve-blueprints-maven-plugin/src/main/java/eve/apol/eveblueprints/BlueprintGraphMojo.java
@@ -4,10 +4,6 @@ import java.io.File;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugins.annotations.Component;
-import org.apache.maven.plugins.annotations.LifecyclePhase;
-import org.apache.maven.plugins.annotations.Mojo;
-import org.apache.maven.plugins.annotations.Parameter;
 import org.sonatype.plexus.build.incremental.BuildContext;
 
 
@@ -16,24 +12,26 @@ import org.sonatype.plexus.build.incremental.BuildContext;
 //@Mojo(name = "generate-graph", defaultPhase = LifecyclePhase.GENERATE_RESOURCES)
 public class BlueprintGraphMojo extends AbstractMojo {
 
-    @Component
     /** @component */
     private BuildContext buildContext;
 
-    @Parameter(defaultValue = "${basedir}/src/main/resources/typeids.csv", property = "typeIDs")
     /** @parameter name="typeIDs" default-value="${basedir}/src/main/resources/typeids.csv" */
     private File typeIDs;
 
-    @Parameter(defaultValue = "${basedir}/src/main/resources/blueprint_graph.csv", property = "typeIDs")
     /** @parameter name="blueprints" default-value="${basedir}/src/main/resources/blueprint_graph.csv" */
     private File blueprints;
 
-    @Parameter(defaultValue = "${project.build.outputDirectory}", property = "outputDir", required = true)
     /** @parameter name="outputDir" default-value="${project.build.outputDirectory}" */
     private File outputDirectory;
 
     public void execute() throws MojoExecutionException {
         File blueprintsGraphFile = new File(outputDirectory, "blueprints.gryo");
+        boolean upToDate = buildContext.isUptodate(blueprintsGraphFile, typeIDs) && buildContext.isUptodate(blueprintsGraphFile, blueprints);
+        if (upToDate) {
+            getLog().info("typeIDs and blueprints up-to-date");
+            return;
+        }
+        
         getLog().info("trying to write into " + blueprintsGraphFile);
         getLog().info("directory exists: " + outputDirectory.exists());
         
